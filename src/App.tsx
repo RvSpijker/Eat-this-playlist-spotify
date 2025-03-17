b6a84e0d205ee929dcd2a648697e6c9ac4613f2f
RvSpijker <robinvanspijker@gmail.com>
2025-03-17 16:30:53 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { AUTH_ENDPOINT, RESPONSE_TYPE, SPOTIFY_CLIENT_ID, SCOPES, REDIRECT_URI } from './config/spotify'
+
+function App() {
+  const [token, setToken] = useState<string | null>(null)
+  const [currentTrack, setCurrentTrack] = useState<any>(null)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const hash = window.location.hash
+    if (hash) {
+      const token = hash.substring(1).split('&').find(elem => elem.startsWith('access_token'))?.split('=')[1]
+      if (token) {
+        setToken(token)
+        window.location.hash = ''
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    const fetchCurrentTrack = async () => {
+      if (!token) return
+
+      try {
+        const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+
+        if (response.status === 204) {
+          setError('No track currently playing')
+          return
+        }
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch current track')
+        }
+
+        const data = await response.json()
+        setCurrentTrack(data)
+        setError(null)
+      } catch (err) {
+        setError('Error fetching current track')
+        console.error(err)
+      }
+    }
+
+    if (token) {
+      fetchCurrentTrack()
+      const interval = setInterval(fetchCurrentTrack, 5000)
+      return () => clearInterval(interval)
+    }
+  }, [token])
+
+  const loginUrl = `${AUTH_ENDPOINT}?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES.join('%20')}`
+
+  return (
+    <div className="App">
+      <h1>Spotify Now Playing</h1>
+      {!token ? (
+        <a href={loginUrl} className="login-button">Login with Spotify</a>
+      ) : (
+        <div className="now-playing">
+          {error ? (
+            <p className="error">{error}</p>
+          ) : currentTrack ? (
+            <div className="track-info">
+              <img 
+                src={currentTrack.item.album.images[0].url} 
+                alt={currentTrack.item.album.name}
+                className="album-cover"
+              />
+              <p>{currentTrack.item.name} - {currentTrack.item.artists[0].name}</p>
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ src/config/spotify.ts
@@ -0,0 +1,9 @@
+export const SPOTIFY_CLIENT_ID = "89b3ecad942e43fb8d6e9a492eefc8d9";
+export const REDIRECT_URI = "http://localhost:5173/callback";
+export const SCOPES = [
+  "user-read-currently-playing",
+  "user-read-playback-state"
+];
+
+export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+export const RESPONSE_TYPE = "token";
