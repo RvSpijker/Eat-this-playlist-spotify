a3e353be908b749ea211de4ab683f763e46be33a
RvSpijker <robinvanspijker@gmail.com>
2025-03-18 17:09:35 +0100

    local fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/eatthisplaylist/',
+  base: process.env.NODE_ENV === 'production' ? '/eatthisplaylist/' : '/',
   build: {
     assetsDir: 'assets',
     rollupOptions: {
